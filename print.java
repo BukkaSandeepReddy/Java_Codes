fc7f3d2ecca728c4708d2f1b1acb983f6a40a5eb
Bukka Sandeep Reddy <bukkasandeepreddy1918@gmail.com>
2025-05-28 21:18:21 +0530

    1st code - Print Code Commited

---
+++ print.java
@@ -0,0 +1,10 @@
+// Importing standard Java library (not strictly needed for this simple case, but shown for clarity)
+import java.lang.System;
+
+public class PrintExample {
+    public static void main(String[] args) {
+        // Using System.out.println from java.lang package
+        System.out.println("Hello, World!");
+    }
+}
+
