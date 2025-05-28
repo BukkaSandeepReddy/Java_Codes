900949f7432aa17c8e3f01a5ef1af7a9591c89d7
Bukka Sandeep Reddy <bukkasandeepreddy1918@gmail.com>
2025-05-28 21:47:47 +0530

    Addition code - 3+7

---
+++ addition.java
@@ -0,0 +1,10 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 7;
+        int sum = a + b;
+
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
+
